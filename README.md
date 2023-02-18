12567965aa8b7de99c770edcd2ec7094b54e7aee
kongaman <christian.kranz@gmail.com>
2023-02-18 22:07:15 +0100

    replacing hardcoded parameters with constants

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-# Jdbc Examples and Best Practices
+# JDBC Examples and Best Practices
 
-Examples an best pratices to connect to databases in java.
+Examples and best practices to connect to databases in java.
 
 ##### Resources used
 
 * sqlite-jdbc-3.40.1.0.jar
 * DB browser for SQLite 3.12.2
+
+##### nice to know/remember
+
+* **Statement.execute()** returns a boolean which is true if the return type is ResultSet and false for everything else. An UPDATE-Statement would return false for example because it returns the number of updates rows and not a ResultStet.
+* Getting the **columns by name** from the ResultSet is ok but using the index is faster. Best Practice is using constants.
--- src/best/practice/Main.java
+++ src/best/practice/Main.java
@@ -7,38 +7,49 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Main {
+	public static final String DB_NAME = "testjava.db";
+	public static final String CONNECTION_STRING = "jdbc:sqlite:H:\\Databases\\" + DB_NAME;
+	public static final String TABLE_CONTACTS = "contacts";
+	public static final String COLUMN_NAME = "name";
+	public static final String COLUMN_PHONE = "phone";
+	public static final String COLUMN_EMAIL = "email";
 
 	public static void main(String[] args) {
-//		//try with resources:
-//		try (Connection conn = DriverManager.getConnection("jdbc:sqlite:H:\\Databases\\testjava.db");
-//		     Statement statement = conn.createStatement()){ }
-//		//this will close the resources in () automatically when the try/catch block is exited.
-		
+
 		try {
-			Connection connection = DriverManager.getConnection("jdbc:sqlite:H:\\Databases\\testjava.db");
-//			connection.setAutoCommit(false);
+			Connection connection = DriverManager.getConnection(CONNECTION_STRING);
 			Statement statement = connection.createStatement();
-			statement.execute("CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)");
-			
-//			statement.execute("INSERT INTO contacts (name, phone, email) VALUES ('Tim', 654321876, 'tim@email.com')");
-//			statement.execute("INSERT INTO contacts (name, phone, email) VALUES ('Chris', 123456789, 'chris@email.com')");
-//			statement.execute("INSERT INTO contacts (name, phone, email) VALUES ('Yvonne', 564738291, 'yc@email.com')");
-//			statement.execute("INSERT INTO contacts (name, phone, email) VALUES ('Fido', 12334, 'dog@email.com')");
-			
-//			statement.execute("UPDATE contacts SET phone = 11223344 WHERE name = 'Chris'");
-//			statement.execute("DELETE FROM contacts WHERE name = 'Yvonne'");
-			
-			statement.execute("SELECT * FROM contacts");
-			ResultSet results = statement.getResultSet();
+
+			statement.execute("DROP TABLE IF EXISTS " + TABLE_CONTACTS);
+
+			statement.execute("CREATE TABLE IF NOT EXISTS " + TABLE_CONTACTS + "(" + COLUMN_NAME + " TEXT, "
+					+ COLUMN_PHONE + " INTEGER, " + COLUMN_EMAIL + " TEXT)");
+
+			statement.execute("INSERT INTO " + TABLE_CONTACTS + " (" + COLUMN_NAME + ", " + COLUMN_PHONE + ", "
+					+ COLUMN_EMAIL + ") " + "VALUES ('Tim', 654321876, 'tim@email.com')");
+			statement.execute("INSERT INTO " + TABLE_CONTACTS + " (" + COLUMN_NAME + ", " + COLUMN_PHONE + ", "
+					+ COLUMN_EMAIL + ") " + "VALUES ('Chris', 123456789, 'chris@email.com')");
+			statement.execute("INSERT INTO " + TABLE_CONTACTS + " (" + COLUMN_NAME + ", " + COLUMN_PHONE + ", "
+					+ COLUMN_EMAIL + ") " + "VALUES ('Yvonne', 564738291, 'yc@email.com')");
+			statement.execute("INSERT INTO " + TABLE_CONTACTS + " (" + COLUMN_NAME + ", " + COLUMN_PHONE + ", "
+					+ COLUMN_EMAIL + ") " + "VALUES ('Fido', 12334, 'dog@email.com')");
+
+			statement.execute("UPDATE " + TABLE_CONTACTS + " SET " + COLUMN_PHONE + " = 11223344 WHERE " + COLUMN_NAME
+					+ " = 'Chris'");
+
+			statement.execute("DELETE FROM " + TABLE_CONTACTS + " WHERE " + COLUMN_NAME + " = 'Fido'");
+
+			ResultSet results = statement.executeQuery("SELECT * FROM contacts");
 			while (results.next()) {
-				System.out.println(results.getString("name") + " " + results.getInt("phone") + " " + results.getString("email"));
+				System.out.println(results.getString(COLUMN_NAME) + " " + results.getInt(COLUMN_PHONE) + " "
+						+ results.getString(COLUMN_EMAIL));
 			}
-			
+
 			results.close();
-			
+
 			statement.close();
 			connection.close();
-				
+
 		} catch (SQLException e) {
 			System.err.println("Something went wrong: " + e.getMessage());
 		}
