c9752e8a180981738f45b939b8dc9c98d8d52b14
kongaman <christian.kranz@gmail.com>
2023-02-18 19:50:36 +0100

    initial commit

---
+++ src/best/practice/Main.java
@@ -0,0 +1,10 @@
+package best.practice;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author kranz
+ *
+ */
+module JdbcExamples {
+}
