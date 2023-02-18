36e154da6f3a2b263b02f09e7652af4e05f2ea71
kongaman <christian.kranz@gmail.com>
2023-02-18 20:48:20 +0100

    Basic connection
    try with resources is commented out. Throughout this course the
    non-try-with-resoureces is used to get into the habbit of closing
    resources after we finished using them

--- src/best/practice/Main.java
+++ src/best/practice/Main.java
@@ -1,10 +1,30 @@
 package best.practice;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//		//try with resources:
+//		try (Connection conn = DriverManager.getConnection("jdbc:sqlite:H:\\Databases\\testjava.db");
+//		     Statement statement = conn.createStatement()){ }
+//		//this will close the resources in () automatically when the try/catch block is exited.
+		
+		try {
+			Connection connection = DriverManager.getConnection("jdbc:sqlite:H:\\Databases\\testjava.db");
+			Statement statement = connection.createStatement();
+			statement.execute("CREATE TABLE IF NOT EXISTS contacts "
+							+ "(name TEXT, phone INTEGER, email TEXT)");
+			
+			statement.close();
+			connection.close();
+				
+		} catch (SQLException e) {
+			System.err.println("Something went wrong: " + e.getMessage());
+		}
 	}
 
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,3 @@
 module JdbcExamples {
+	requires java.sql;
 }
