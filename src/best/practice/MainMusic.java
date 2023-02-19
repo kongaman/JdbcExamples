8570393600ab4b704e4cbba2c3f0de945a1d2030
kongaman <christian.kranz@gmail.com>
2023-02-19 11:50:36 +0100

    refining the query

--- src/best/practice/MainMusic.java
+++ src/best/practice/MainMusic.java
@@ -14,7 +14,7 @@ public class MainMusic {
 			return;
 		}
 		
-		List<Artist> artists = datasource.queryArtists();
+		List<Artist> artists = datasource.queryArtists(Datasource.ORDER_BY_ASC);
 		if (artists == null) {
 			System.out.println("No artists!");
 			return;
