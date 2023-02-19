2a9b5631e584efe230c10ccc7712745f229a613a
kongaman <christian.kranz@gmail.com>
2023-02-19 15:31:26 +0100

    Using constants in all artists Query

--- src/best/practice/model/Datasource.java
+++ src/best/practice/model/Datasource.java
@@ -41,6 +41,9 @@ public class Datasource {
 	public static final int ORDER_BY_ASC = 2;
 	public static final int ORDER_BY_DESC = 3;
 	
+	public static final String QUERY_ARTISTS_START = "SELECT * FROM " + TABLE_ARTISTS;
+	public static final String QUERY_ARTISTS_SORT = " ORDER BY " + COLUMN_ARTIST_NAME + " COLLATE NOCASE ";
+	
 	public static final String QUERY_ALBUMS_BY_ARTIST_START  = "SELECT " + TABLE_ALBUMS + "." + COLUMN_ALBUM_NAME + " FROM "
 			+ TABLE_ALBUMS + " INNER JOIN " + TABLE_ARTISTS + " ON " + TABLE_ALBUMS + "." + COLUMN_ALBUM_ARTIST + " = "
 			+ TABLE_ARTISTS + "." + COLUMN_ARTIST_ID + " WHERE " + TABLE_ARTISTS + "." + COLUMN_ARTIST_NAME + " = \"";
@@ -70,12 +73,9 @@ public class Datasource {
 	
 	public List<Artist> queryArtists(int sortOrder) {
 		
-		StringBuilder sb = new StringBuilder("SELECT * FROM ");
-		sb.append(TABLE_ARTISTS);
+		StringBuilder sb = new StringBuilder(QUERY_ARTISTS_START);
 		if (sortOrder != ORDER_BY_NONE) {
-			sb.append(" ORDER BY ");
-			sb.append(COLUMN_ARTIST_NAME);
-			sb.append(" COLLATE NOCASE ");
+			sb.append(QUERY_ARTISTS_SORT);
 			sb.append((sortOrder == ORDER_BY_DESC) ? "DESC" : "ASC");
 		}
 		
